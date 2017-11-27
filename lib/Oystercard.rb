4747e0ce32f379287023b5950dd2ca6c271868bc
Léa R <lea.rosamond@gmail.com>
2017-11-27 15:47:56 +0000

    raising error if more than 90 pounds

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,13 +1,16 @@
 
 class Oystercard
 
-  attr_reader :balance
+  attr_accessor :balance
+  MAX = 90
 
    def initialize
      @balance = 0
+
    end
 
    def top_up(chosen_value)
-     @balance += chosen_value
+     raise "You're too wealthy" if @balance + chosen_value >= MAX
+     @balance = @balance + chosen_value
    end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,9 +8,13 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
     it 'top up money when desired' do
-      chosen_value = 1000
-      expect(subject.top_up(chosen_value)).to eq(1000)
+      chosen_value = 20
+      expect(subject.top_up(chosen_value)).to eq(20)
     end
-
+    it 'raise error if more than 90 quid' do
+      card = Oystercard.new
+      card.balance = 89
+      expect{ card.top_up(2) }.to raise_error "You're too wealthy"
+    end
   end
 end
