444f916cf66f1302ff986f7c1e7f859395bf073f
Léa R <lea.rosamond@gmail.com>
2017-11-28 14:55:11 +0000

    Adding a minimum fare and deducting it as a touch out

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,8 +1,10 @@
 class Oystercard
 
   attr_accessor :balance
-  attr_accessor :in_journey
+  # attr_accessor :in_journey
   MAX = 90
+  MIN = 1
+  MIN_CHARGE = 1
 
    def initialize
      @balance = 0
@@ -10,25 +12,31 @@ class Oystercard
    end
 
    def top_up(chosen_value)
-     raise "You're too wealthy, not more #{Oystercard::MAX}" if @balance + chosen_value >= MAX
-     @balance + chosen_value
+     raise "You're too wealthy, not more #{Oystercard::MAX}" if @balance + chosen_value > MAX
+     @balance += chosen_value
+
    end
 
-   def deduct(value)
-     @balance - value
-   end
+
 
    def touch_in
+      raise "You don't have enough money" if @balance < MIN
       @in_journey = true
-
    end
 
    def touch_out
      @in_journey = false
+     deduct(MIN_CHARGE)
    end
 
    def in_journey?
      @in_journey
+   end
+
+  
+
+   def deduct(value)
+     @balance -= value
    end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'Oystercard'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new}
 
   context 'balance' do
 
@@ -8,13 +9,18 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
     it 'top up money when desired' do
-      chosen_value = 20
-      expect(subject.top_up(chosen_value)).to eq(20)
+      expect {oystercard.top_up(10)}.to change{oystercard.balance}.by(10)
     end
     it 'raise error if more than 90 quid' do
-      card = Oystercard.new
-      card.balance = 89
-      expect{ card.top_up(2) }.to raise_error "You're too wealthy, not more #{Oystercard::MAX}"
+      oystercard.top_up(Oystercard::MAX)
+      expect{ oystercard.top_up(1) }.to raise_error "You're too wealthy, not more #{Oystercard::MAX}"
+    end
+    it 'raise error if less than 1 pound' do
+      expect{ oystercard.touch_in }.to raise_error "You don't have enough money"
+    end
+    it 'deduct money from the card' do
+      oystercard.top_up(Oystercard::MIN)
+      expect{oystercard.deduct(10)}.to change{oystercard.balance}.by(-10)
     end
 
     it 'reduces set amount of money when asked' do
@@ -25,21 +31,30 @@ describe Oystercard do
   end
 
   context 'card.in_journey?' do
+
     it {is_expected.to respond_to :in_journey?}
   end
 
   context 'after touch_in' do
-    it 'should be true ' do
-      card = Oystercard.new
-      expect(touch_in(card)).to eq (card.in_journey = true)
+
+    it 'the card should be in journey ' do
+      oystercard.top_up(Oystercard::MIN)
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
     end
   end
 
-    context 'after touch_out' do
-      it 'should be false' do
-        card = Oystercard.new
-        expect(touch_out(card)).to eq (card.in_journey = false)
-      end
+  context 'after touch_out' do
+
+    it 'the card should not be in journey' do
+      oystercard.touch_out
+      expect(oystercard).not_to be_in_journey
+    end
+
+    it 'deduct money from card' do
+      oystercard.top_up(Oystercard::MIN)
+      expect{oystercard.touch_out}.to change{oystercard.balance}.by(-Oystercard::MIN_CHARGE)
     end
   end
+
 end
